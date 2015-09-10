cf61806a9844df403d022a0f68e2867e4e823b53
Juho Friman <juho.t.friman@gmail.com>
2015-09-10 09:05:48 +0300

    Quick fix for trying to fix build in travis. Real problem is that public api of porwigle.core is kinda tacky at the moment.

--- test/porwigle/handler/public_test.clj
+++ test/porwigle/handler/public_test.clj
@@ -7,7 +7,8 @@
 
   (testing "When page is non existent 404 with empty body is expected"
 
-    (with-redefs [porwigle.core/eval-page (fn [_] "")]
+    (with-redefs [porwigle.core/eval-page (fn [_] "")
+                  porwigle.core/pagestructure (fn [_] {:content ""})]
 
       (let [response (handle-request {:uri "/non-existent"})]
         (is (= 404 (:status response)))
@@ -15,7 +16,8 @@
 
   (testing "When page exists 200 with contents as body is expected"
 
-    (with-redefs [porwigle.core/eval-page (fn [_] "body")]
+    (with-redefs [porwigle.core/eval-page (fn [_] "body")
+                  porwigle.core/pagestructure (fn [_] {:content "body"})]
 
       (let [response (handle-request {:uri "/existing"})]
         (is (= 200 (:status response)))
